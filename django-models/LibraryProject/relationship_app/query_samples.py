df1db51eedc720cb3e883fac668f2534f8232cc3
AmrHishamFarouk <hishamamr121@gmail.com>
2024-11-17 20:13:05 +0200

    third commit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,9 @@
 #query books by author
-author = Author.objects.get(name='author_name')
+author = Author.objects.get(name=author_name)
 books_by_author = author.books.all() 
 #all books
-library = Library.objects.get(name='Library_name')
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 #librarian
-library = Library.objects.get(name='Library_name') 
+library = Library.objects.get(name=Library_name) 
 librarian = library.librarian
